f46049cda17fc1b829a98c2f7926e630c4e8d255
Chexovpert <chexovpert@gmail.com>
2021-05-06 15:40:04 +0300

    refactor: apply prettier

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const chalk = require('chalk')
-const text = require('./data')
-const fs = require('fs')
-const path = require('path')
+const chalk = require("chalk");
+const text = require("./data");
+const fs = require("fs");
+const path = require("path");
 
 //console.log(chalk.blue(text))
 //console.log(__dirname)
@@ -23,82 +23,92 @@ const path = require('path')
 const inputpath = path.join(__dirname, "files", "input.txt");
 const outputpath = path.join(__dirname, "files", "output.txt");
 const newpath = path.join(__dirname, "files", "newpath.txt");
-const createfile= function() {
-    fs.writeFile(inputpath, "hello node" , (err) => {
-        if (err) {
-            throw err
-        }
-        //console.log("file has been created")
-    })
-    fs.appendFile(inputpath, "\nhello again" , (err) => {
-        if (err) {
-            throw err
-        }
-        //console.log("file has been updated")
-    })
-}
-
+const createfile = function () {
+  fs.writeFile(inputpath, "hello node", (err) => {
+    if (err) {
+      throw err;
+    }
+    //console.log("file has been created")
+  });
+  fs.appendFile(inputpath, "\nhello again", (err) => {
+    if (err) {
+      throw err;
+    }
+    //console.log("file has been updated")
+  });
+};
+console.log();
 const encryption = (shift) => {
-    if (shift > 26) {
-        shift = shift%26
+  if (shift > 26) {
+    shift = shift % 26;
+  }
+  fs.readFile(inputpath, "utf-8", (err, content) => {
+    if (err) {
+      throw err;
     }
-    fs.readFile(inputpath,"utf-8", (err, content) => {
-        if (err) {
-            throw err
-        }
-        console.log(content);
-        const inputdata= content.toLowerCase().split('')
-        const encrypteddata = []
-        inputdata.forEach((elem) => {
-            if (elem.charCodeAt(0) && (elem.charCodeAt(0)>=65 && elem.charCodeAt(0)<=90)|| (elem.charCodeAt(0)>=97 && elem.charCodeAt(0)<=122)) {
-                elem=String.fromCharCode(elem.charCodeAt(0)+shift)
-                encrypteddata.push(elem)
-                return
-                //console.log(elem)
-            }
-            encrypteddata.push(elem)
-        })
-        console.log(encrypteddata.join(""));
-        const encrypteddatatext=encrypteddata.join("")
-        fs.writeFile(outputpath, encrypteddatatext, (err) => {
-            if(err) {
-                throw err
-            }
-            console.log('file has been encrypted');
-        })
-    })
-}
+    console.log(content);
+    const inputdata = content.toLowerCase().split("");
+    const encrypteddata = [];
+    inputdata.forEach((elem) => {
+      if (
+        (elem.charCodeAt(0) &&
+          elem.charCodeAt(0) >= 65 &&
+          elem.charCodeAt(0) <= 90) ||
+        (elem.charCodeAt(0) >= 97 && elem.charCodeAt(0) <= 122)
+      ) {
+        elem = String.fromCharCode(elem.charCodeAt(0) + shift);
+        encrypteddata.push(elem);
+        return;
+        //console.log(elem)
+      }
+      encrypteddata.push(elem);
+    });
+    console.log(encrypteddata.join(""));
+    const encrypteddatatext = encrypteddata.join("");
+    fs.writeFile(outputpath, encrypteddatatext, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("file has been encrypted");
+    });
+  });
+};
 const decryption = (shift) => {
-    if (shift > 26) {
-        shift = shift%26
+  if (shift > 26) {
+    shift = shift % 26;
+  }
+  fs.readFile(outputpath, "utf-8", (err, content) => {
+    if (err) {
+      throw err;
     }
-    fs.readFile(outputpath,"utf-8", (err, content) => {
-        if (err) {
-            throw err
-        }
-        console.log(content);
-        const inputdata= content.toLowerCase().split('')
-        const encrypteddata = []
-        inputdata.forEach((elem) => {
-            if (elem.charCodeAt(0) && (elem.charCodeAt(0)>=65 && elem.charCodeAt(0)<=90)|| (elem.charCodeAt(0)>=97 && elem.charCodeAt(0)<=122)) {
-                elem=String.fromCharCode(elem.charCodeAt(0)-shift)
-                encrypteddata.push(elem)
-                return
-                //console.log(elem)
-            }
-            encrypteddata.push(elem)
-        })
-        console.log(encrypteddata.join(""));
-        const encrypteddatatext=encrypteddata.join("")
-        fs.writeFile(newpath, encrypteddatatext, (err) => {
-            if(err) {
-                throw err
-            }
-            console.log('file has been decrypted');
-        })
-    })
-}
+    console.log(content);
+    const inputdata = content.toLowerCase().split("");
+    const encrypteddata = [];
+    inputdata.forEach((elem) => {
+      if (
+        (elem.charCodeAt(0) &&
+          elem.charCodeAt(0) >= 65 &&
+          elem.charCodeAt(0) <= 90) ||
+        (elem.charCodeAt(0) >= 97 && elem.charCodeAt(0) <= 122)
+      ) {
+        elem = String.fromCharCode(elem.charCodeAt(0) - shift);
+        encrypteddata.push(elem);
+        return;
+        //console.log(elem)
+      }
+      encrypteddata.push(elem);
+    });
+    console.log(encrypteddata.join(""));
+    const encrypteddatatext = encrypteddata.join("");
+    fs.writeFile(newpath, encrypteddatatext, (err) => {
+      if (err) {
+        throw err;
+      }
+      console.log("file has been decrypted");
+    });
+  });
+};
 
 //createfile()
 //encryption(53)
-decryption(53)
+//decryption(53)
